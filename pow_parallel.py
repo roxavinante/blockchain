743de8543d3afc57c98f3d09b98650dc79b51324
Rox <rox@asti.dost.gov.ph>
2020-08-09 14:12:52 +0800

    Update pow_parallel.py

--- pow_parallel.py
+++ pow_parallel.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     rank = comm.Get_rank()
     name = MPI.Get_processor_name()
     while True:
-        hash_value = hashlib.sha256(b'{0}{1}'.format(block_header, nonce).encode('utf-8')).hexdigest()
+        hash_value = hashlib.sha256('{0}{1}'.format(block_header, nonce).encode('utf-8')).hexdigest()
         print("Try Nonce: {0} => Hash Result: {1} | Rank: {2}, Size: {3}, Node: {4}".format(nonce, hash_value, rank, size, name))
         nonce += 1
 
